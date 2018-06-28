ee10e298b27496db4d3eb4d2ce9d3c5f945af4a4
loaner <rascarandang@gmail.com>
2018-06-27 23:58:23 -0700

    Updated setup script with upload shorthand

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #/bin/sh
 #download package
 
+echo "alias upload='node upload.js'" >> ~/.bash_profile
+
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
@@ -12,6 +14,6 @@ npm install
 sudo npm install claudia -g
 echo "***********************exporting AWS keys***************************"
 claudia create --region us-west-2 --handler AlexaApp.handler --version skill
-LAMBDA_INFO=claudia allow-alexa-skill-trigger --version skill
+LAMBDA_INFO=$(claudia allow-alexa-skill-trigger --version skill);
 echo $LAMBDA_INFO | sed -n "s/^.*\(arn.*\)\".*$/\1/p" >> ~/Documents/endpoint.txt
 cat ~/Documents/endpoint.txt
