553450ba196fcfb65f39db58f23f88242bddaee1
loaner <rascarandang@gmail.com>
2018-06-27 18:14:44 -0700

    Updated to a new setup script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #/bin/sh
 #download package
 
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+aws configure
+
 #install claudia
 echo "************************installing claudia***************************"
 npm install 
 sudo npm install claudia -g
 echo "***********************exporting AWS keys***************************"
 claudia create --region us-west-2 --handler AlexaApp.handler --version skill
-claudia allow-alexa-skill-trigger --version skill
-
+LAMBDA_INFO=claudia allow-alexa-skill-trigger --version skill
+echo $LAMBDA_INFO | sed -n "s/^.*\(arn.*\)\".*$/\1/p" >> ~/Documents/endpoint.txt
+cat ~/Documents/endpoint.txt
