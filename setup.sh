06a7f47c608f10d33850222465c302904d381e4c
loaner <rascarandang@gmail.com>
2018-06-27 17:20:43 -0700

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#/bin/sh
 #download package
 
 #install claudia
@@ -6,8 +6,6 @@ echo "************************installing claudia***************************"
 npm install 
 sudo npm install claudia -g
 echo "***********************exporting AWS keys***************************"
-./export_aws_key.sh
-source export_aws_key.sh
 claudia create --region us-west-2 --handler AlexaApp.handler --version skill
 claudia allow-alexa-skill-trigger --version skill
 
